1ad6e8768258e1b94c63edfd70598cc2608bb8d3
Alexander Drach <alexander-drach@mail.ru>
2020-09-07 16:43:47 +0600

    add tests

--- src/js/__tests__/matchers.test.js
+++ src/js/__tests__/matchers.test.js
@@ -35,3 +35,53 @@ test('sort test toEqual', () => {
 
   expect(result).toEqual(sortArr);
 });
+
+test('sort test toEqual one element', () => {
+  const arr = [
+    { name: 'мечник', health: 10 },
+  ];
+
+  const sortArr = [
+    { name: 'мечник', health: 10 },
+  ];
+
+  const result = sortPerson(arr);
+
+  expect(result).toEqual(sortArr);
+});
+
+test('sort test toEqual sort', () => {
+  const arr = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const sortArr = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const result = sortPerson(arr);
+
+  expect(result).toEqual(sortArr);
+});
+
+test('sort test toEqual reverse', () => {
+  const arr = [
+    { name: 'мечник', health: 10 },
+    { name: 'лучник', health: 80 },
+    { name: 'маг', health: 100 },
+  ];
+
+  const sortArr = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const result = sortPerson(arr);
+
+  expect(result).toEqual(sortArr);
+});
